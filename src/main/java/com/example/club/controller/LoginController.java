661ec593842565f32332e3a4868c53fd1bd193be
ChuckieTan <tsq1105@gmail.com>
2021-02-09 22:17:25 +0800

    login returns user id

--- src/main/java/com/example/club/controller/LoginController.java
+++ src/main/java/com/example/club/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.club.controller;
 
+import com.example.club.service.UserService;
 import com.example.club.util.Result;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 @RestController
 public class LoginController {
+    @Resource
+    UserService userService;
+
     @PostMapping(value = "/login")
     public Result login(@RequestBody Map<String, String> params) {
         String username = params.get("username");
@@ -22,16 +27,18 @@ public class LoginController {
 
         // 创建用户认证的身份令牌，并设置帐号密码
         var usernamePasswordToken = new UsernamePasswordToken(username, password);
-        Result result = null;
+        Result result;
         try {
             // 如果已登录则登出
             subject.logout();
             subject.login(usernamePasswordToken);
-            result = new Result(1, "login success", null);
+            result = new Result(1,
+                    "登录成功",
+                    userService.queryInfoByNumber(username).getUserId());
         } catch (UnknownAccountException e) {
-            result = new Result(0, "unknown account", null);
+            result = new Result(0, "帐号不存在", null);
         } catch (AuthenticationException e) {
-            result = new Result(0, "username or password was wrong", null);
+            result = new Result(0, "密码错误", null);
         }
         return result;
     }
